0ea0a5ef6270588b9ca0e9d7f7ad3fae695fcb55
Rob Lourens <roblourens@gmail.com>
2018-04-02 16:28:04 -0700

    Show hint when value changes

--- src/components/SettingItem.tsx
+++ src/components/SettingItem.tsx
@@ -44,7 +44,7 @@ function SettingItem(props: ItemProps) {
 
     return (
         <ListItem button={true} className={classes.listItem} >
-            <ListItemText primary={name} secondary={setting.description} className={classes.listItemText} />
+            <ListItemText primary={`"${name}"`} secondary={setting.description} className={classes.listItemText} />
             <ListItemSecondaryAction className={classes.settingValueEditor}>{renderSettingValue(setting, classes)}</ListItemSecondaryAction>
         </ListItem>
     );
@@ -57,38 +57,76 @@ interface ValueProps {
     classes: any;
 }
 
-function BoolSettingValue(props: ValueProps) {
-    return (
-        <Checkbox defaultChecked={props.setting.default} />
-    );
+interface ValueState {
+    value: any;
 }
 
-function StringSettingValue(props: ValueProps) {
-    const inputProps = { className: props.classes.textFieldInput };
-    return (
-        <TextField defaultValue={props.setting.default} inputProps={inputProps} />
-    );
+class SettingValue extends React.Component<ValueProps, ValueState> {
+    protected handleChange() {
+        return event => {
+            this.setState({ 'value': event.target.value });
+        };
+    }
+
+    protected getHelperText() {
+        if (!this.state) {
+            return '';
+        }
+
+        const { setting } = this.props;
+        return (!this.state || setting.default + '' === this.state.value) ?
+            '' :
+            'Modified';
+    }
 }
 
-function NumberSettingValue(props: ValueProps) {
-    const inputProps = { className: props.classes.numberFieldInput };
-    return (
-        <TextField defaultValue={String(props.setting.default)} inputProps={inputProps} />
-    );
+class BoolSettingValue extends SettingValue {
+    render() {
+        const { setting, classes } = this.props;
+        return (
+            <Checkbox defaultChecked={setting.default} onChange={this.handleChange()} />
+        );
+    }
 }
 
-function ObjectSettingValue(props: ValueProps) {
-    return (
-        <TextField disabled={true} value="Edit in settings.json!" />
-    );
+class StringSettingValue extends SettingValue {
+    render() {
+        const { setting, classes } = this.props;
+        const inputProps = { className: classes.textFieldInput };
+        return (
+            <TextField defaultValue={setting.default} inputProps={inputProps} onChange={this.handleChange()} helperText={this.getHelperText()} />
+        );
+    }
 }
 
-function EnumSettingValue(props: ValueProps) {
-    return (
-        <Select value={props.setting.default}>
-            {props.setting.enum!.map(enumStr => <MenuItem key={enumStr} value={enumStr}>{enumStr}</MenuItem>)}
-        </Select>
-    );
+class NumberSettingValue extends SettingValue {
+    render() {
+        const { setting, classes } = this.props;
+        const inputProps = { className: classes.numberFieldInput };
+        return (
+            <TextField defaultValue={String(setting.default)} inputProps={inputProps} onChange={this.handleChange()} helperText={this.getHelperText()} />
+        );
+    }
+}
+
+class ObjectSettingValue extends SettingValue {
+    render() {
+        const { setting, classes } = this.props;
+        return (
+            <TextField disabled={true} value="Edit in settings.json!" onChange={this.handleChange()} />
+        );
+    }
+}
+
+class EnumSettingValue extends SettingValue {
+    render() {
+        const { setting, classes } = this.props;
+        return (
+            <TextField select={true} value={this.state && this.state.value || setting.default} onChange={this.handleChange()} helperText={this.getHelperText()} >
+                {setting.enum!.map(enumStr => <MenuItem key={enumStr} value={enumStr}>{enumStr}</MenuItem>)}
+            </TextField>
+        );
+    }
 }
 
 function renderSettingValue(setting: Setting, classes: any) {
